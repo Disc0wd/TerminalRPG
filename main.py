a39f08c5829a23faa428bce43397c41a31d25299
Disc0wd <igshs12yt@gmail.com>
2025-11-26 09:53:40 +0100

    fixed classes, added hero class (has x and y coordinates( might need to add some leveling etc, but thats for later)), removed old debugging tools(new ones needed)

--- main.py
+++ main.py
@@ -3,18 +3,6 @@ from grid import generate_grid, print_grid
 import random
 
 hero = Character("Hero", 100, 50)
-enemies = [Character("Monster" + str(i), random.randint(80, 120), random.randint(25, 60)) for i in range(5)]
-
-for enemy in enemies:
-    print(enemy)
-
-choice = input("Which monster do you want to target? ")
-
-target = next((e for e in enemies if e.name == choice), None)
-
-if target:
-    hero.cast_spell(30, target)
-    target.cast_spell(30, hero)
 
 # create grid
 grid = generate_grid()
